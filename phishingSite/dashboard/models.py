e2ea8e6f967d0116ca42b1c4d22aa1f874a7ecdf
Liinnaa <lina.mya.taki@gmail.com>
2023-11-17 17:20:24 +0100

    update db

--- phishingSite/dashboard/models.py
+++ phishingSite/dashboard/models.py
@@ -2,19 +2,30 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
+# Table contenant les entreprises
+class entreprise(models.Model):
+    nomEntreprise = models.CharField(max_length=200)
+
+# Table contenant les employés de chaque entreprise
 class employee(models.Model):
     nom = models.CharField(max_length=200)
     mail_address = models.EmailField()
-    entreprise = models.CharField(max_length=200)
+    id_entreprise = models.ForeignKey(entreprise, on_delete=models.Cascade)
 
     def __str__(self):
         return self.mail_address
 
+# Table contenant les campagnes créées par les utilisateurs
 class campagne(models.Model):
-    entreprise = models.CharField(max_length=200)
+    id_entreprise = models.ForeignKey(entreprise, on_delete=models.Cascade)
     mailEnvoi = models.EmailField()
     password_mailEnvoi = models.PasswordField()
 
+    def save(self, *args, **kwargs):
+        self.password = make_password(self.password)
+        super().save(*args, **kwargs)
+
+# Table contenant la liste des emails envoés lors des campagnes
 class emailCampagne(models.Model):
     id_campagne = models.ForeignKey(campagne, on_delete=models.Cascade)
     id_employee = models.ForeignKey(employee, on_delete=models.Cascade)
