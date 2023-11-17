4747f16ce5c737c611d46aa2ba18f0fd57fe1acd
Liinnaa <lina.mya.taki@gmail.com>
2023-11-17 16:31:32 +0100

    Creation bdd

--- phishingSite/dashboard/models.py
+++ phishingSite/dashboard/models.py
@@ -3,9 +3,20 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class employee(models.Model):
-    name = models.CharField(max_length=200)
+    nom = models.CharField(max_length=200)
     mail_address = models.EmailField()
-    received_at = models.DateTimeField(auto_now_add=True)
+    entreprise = models.CharField(max_length=200)
 
     def __str__(self):
         return self.mail_address
+
+class campagne(models.Model):
+    entreprise = models.CharField(max_length=200)
+    mailEnvoi = models.EmailField()
+    password_mailEnvoi = models.PasswordField()
+
+class emailCampagne(models.Model):
+    id_campagne = models.ForeignKey(campagne, on_delete=models.Cascade)
+    id_employee = models.ForeignKey(employee, on_delete=models.Cascade)
+    clicked = models.BooleanField(default=False)
+    form_completed = models.BooleanField(default=False)
