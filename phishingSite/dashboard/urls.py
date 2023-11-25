a2615c128b51e687b796a498c40f1fee929fa541
Melinda Marmot <melinda.marmot@epita.fr>
2023-11-25 11:55:11 +0100

    Interface

--- phishingSite/dashboard/urls.py
+++ phishingSite/dashboard/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import dashboard, employee_list, register, statistics, create_campaign
+from .views import dashboard, employee_list, register, statistics, create_campaign, phishing_mail_generator, phishing_page_generator, param_admin, Info, render_chart
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
@@ -11,4 +11,9 @@ urlpatterns = [
     path("statistics", statistics, name="statistics"),
     path("create-campaign", create_campaign, name="create_campaign"),
     path('employee-list', employee_list, name="employee_list"),
+    path('phishing_mail_generator', phishing_mail_generator, name="phishing_mail_generator"),
+    path('phishing_page_generator', phishing_page_generator, name="phishing_page_generator"),
+    path('param_admin', param_admin, name="param_admin"),
+    path('Info', Info, name="Info"),
+    path('render_chart/', render_chart, name='render_chart'),
 ]
