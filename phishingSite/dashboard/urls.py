7bfdebc3b4b270d99ab2acfc6549e9199aa5f2c6
raph-houver <lina.mya.taki@gmail.com>
2023-11-26 14:06:48 +0100

    email campagne view

--- phishingSite/dashboard/urls.py
+++ phishingSite/dashboard/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import dashboard, employee_list, entreprise_list, campagne_list, register, statistics, create_campaign
+from .views import dashboard, employee_list, entreprise_list, campagne_list, emailcampagne_list register, statistics, create_campaign
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
@@ -13,4 +13,6 @@ urlpatterns = [
     path('employee-list', employee_list, name="employee_list"),
     path('entreprise-list/', entreprise_list, name="entreprise_list"),
     path('campagne-list/', campagne_list, name="campagne_list"),
+    path('emailcampagne-list/', emailcampagne_list, name="emailcampagne_list"),
 ]
+
