be970fb8fb302eaf474adf5e1abc0a2ccb067895
raph-houver <lina.mya.taki@gmail.com>
2023-11-26 14:00:00 +0100

    test

--- phishingSite/dashboard/views.py
+++ phishingSite/dashboard/views.py
@@ -37,3 +37,4 @@ def entreprise_list(request):
 def campagne_list(request):
     campagnes = campagne.objects.all()
     return render(request, 'dashboard/campagne_list.html', {'campagnes':campagnes})
+    test
