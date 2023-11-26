058715d89794a0b40eb974edc0191b47d778cc8a
Raphael Houver <raphael.houver@efficientip.com>
2023-11-26 20:50:36 +0100

    dont know

--- phishingSite/info/views.py
+++ phishingSite/info/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
-from dashboard.models import emailCampagne
+from dashboard.models import EmailCampagne
 
 def login(request):
     email_campagne_id = request.GET.get('email_campagne_id')
     context = {'email_campagne_id': email_campagne_id}
     #entreprise = request.GET.get('nomEntreprise')
     if email_campagne_id:
-        instance = get_object_or_404(emailCampagne, id=email_campagne_id)
+        instance = get_object_or_404(EmailCampagne, id=email_campagne_id)
         instance.clicked = True
         instance.save()
         print(f"{email_campagne_id} a cliqué sur le lien comme un gros nul")
@@ -17,7 +17,7 @@ def submit(request):
     email_campagne_id = request.POST.get('email_campagne_id')
     context = {'email_campagne_id': email_campagne_id}
     if email_campagne_id:
-        instance = get_object_or_404(emailCampagne, id=email_campagne_id)
+        instance = get_object_or_404(EmailCampagne, id=email_campagne_id)
         instance.form_completed = True
         instance.save()
         print(f"{email_campagne_id} s'est connecté")
