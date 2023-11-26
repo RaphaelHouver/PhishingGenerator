82ea6622b57ee4c405503f8597550ac976387d8f
raph-houver <lina.mya.taki@gmail.com>
2023-11-26 17:17:49 +0100

    enregistrement dans bdd lorsque form rempli

--- phishingSite/dashboard/admin.py
+++ phishingSite/dashboard/admin.py
@@ -4,8 +4,16 @@ from .models import campagne
 from .models import entreprise
 from .models import emailCampagne
 
+# class campagneAdmin(admin.ModelAdmin):
+#     list_display = ('id', 'id_entreprise', 'mailEnvoi', 'password_mailEnvoi', 'custom_edit_link')
+#     def custom_edit_link(self, obj):
+#         return f'<a href="/admin/dashboard/campagne/{obj.id}/change/">Edit</a>'
+
+#     custom_edit_link.allow_tags = True
+#     custom_edit_link.short_description = 'Edit Link'
 
 admin.site.register(employee)
+# admin.site.register(campagne, campagneAdmin)
 admin.site.register(campagne)
 admin.site.register(entreprise)
 admin.site.register(emailCampagne)
--- phishingSite/info/views.py
+++ phishingSite/info/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from dashboard.models import emailCampagne
 
 def login(request):
     email_campagne_id = request.GET.get('email_campagne_id')
     context = {'email_campagne_id': email_campagne_id}
     #entreprise = request.GET.get('nomEntreprise')
     if email_campagne_id:
-        #email_campagne = EmailCampagne.objects.get(idEmail=email_campagne_id)
-        #email_campagne.clicked = True
-        #email_campagne.save()
+        instance = get_object_or_404(emailCampagne, id=email_campagne_id)
+        instance.clicked = True
+        instance.save()
         print(f"{email_campagne_id} a cliqué sur le lien comme un gros nul")
     return render(request, 'info/microsoft/login.html', context)
 
@@ -15,8 +17,8 @@ def submit(request):
     email_campagne_id = request.POST.get('email_campagne_id')
     context = {'email_campagne_id': email_campagne_id}
     if email_campagne_id:
-        # email_campagne = EmailCampagne.objects.get(idEmail=email_campagne_id)
-        # email_campagne.form_completed = True
-        # email_campagne.save()
+        instance = get_object_or_404(emailCampagne, id=email_campagne_id)
+        instance.form_completed = True
+        instance.save()
         print(f"{email_campagne_id} s'est connecté")
     return render(request, 'info/microsoft/submit.html', context)
